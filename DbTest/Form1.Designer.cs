8c55c574079abd6c2e1ce0e2443d0b9f8b2c0710
letnull19a <alexvolkovdd@gmail.com>
2023-10-30 21:41:35 +0400

    Добавьте файлы проекта.

---
+++ DbTest/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace DbTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            textBoxName = new TextBox();
+            textBoxSurname = new TextBox();
+            textBoxFatherName = new TextBox();
+            textBoxLogin = new TextBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(776, 338);
+            dataGridView1.TabIndex = 0;
+            // 
+            // textBoxName
+            // 
+            textBoxName.Location = new Point(12, 386);
+            textBoxName.Name = "textBoxName";
+            textBoxName.Size = new Size(100, 23);
+            textBoxName.TabIndex = 1;
+            // 
+            // textBoxSurname
+            // 
+            textBoxSurname.Location = new Point(118, 386);
+            textBoxSurname.Name = "textBoxSurname";
+            textBoxSurname.Size = new Size(100, 23);
+            textBoxSurname.TabIndex = 2;
+            // 
+            // textBoxFatherName
+            // 
+            textBoxFatherName.Location = new Point(224, 386);
+            textBoxFatherName.Name = "textBoxFatherName";
+            textBoxFatherName.Size = new Size(100, 23);
+            textBoxFatherName.TabIndex = 3;
+            // 
+            // textBoxLogin
+            // 
+            textBoxLogin.Location = new Point(330, 386);
+            textBoxLogin.Name = "textBoxLogin";
+            textBoxLogin.Size = new Size(100, 23);
+            textBoxLogin.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(436, 386);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 5;
+            button1.Text = "Добавить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBoxLogin);
+            Controls.Add(textBoxFatherName);
+            Controls.Add(textBoxSurname);
+            Controls.Add(textBoxName);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private TextBox textBoxName;
+        private TextBox textBoxSurname;
+        private TextBox textBoxFatherName;
+        private TextBox textBoxLogin;
+        private Button button1;
+    }
+}
---
+++ DbTest/Form1.cs
@@ -0,0 +1,62 @@
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DbTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var connection = new SqlConnection("Data Source=DESKTOP-EDPUPHE\\SQLEXPRESS; Initial Catalog=TestDB; Integrated Security=true; TrustServerCertificate=True;");
+
+            connection.Open();
+
+            var adapter = new SqlDataAdapter("SELECT * FROM users", connection);
+
+            var dataSet = new DataSet();
+
+            adapter.Fill(dataSet);
+
+            dataGridView1.DataSource = dataSet.Tables[0];
+
+            connection.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var connection = new SqlConnection("Data Source=DESKTOP-EDPUPHE\\SQLEXPRESS; Initial Catalog=TestDB; Integrated Security=true; TrustServerCertificate=True;"))
+            {
+                // вставка
+                connection.Open();
+                var login = new SqlParameter("@Login", textBoxLogin.Text);
+                var name = new SqlParameter("@Name", textBoxName.Text);
+                var surname = new SqlParameter("@Surname", textBoxSurname.Text);
+                var fatherName = new SqlParameter("@FatherName", textBoxFatherName.Text);
+
+                var query = new SqlCommand("INSERT INTO users (name, surname, fatherName, login) VALUES (@Name, @Surname, @FatherName, @Login)", connection);
+
+                query.Parameters.Add(login);
+                query.Parameters.Add(name);
+                query.Parameters.Add(surname);
+                query.Parameters.Add(fatherName);
+
+                query.ExecuteNonQuery();
+
+                // обновление
+                var adapter = new SqlDataAdapter("SELECT * FROM users", connection);
+
+                var dataSet = new DataSet();
+
+                adapter.Fill(dataSet);
+
+                dataGridView1.DataSource = dataSet.Tables[0];
+                connection.Close();
+            }
+        }
+    }
+}
